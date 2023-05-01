1e905d467ce368437c5a87deed0b146a161bcaf5
azufb <curiousazunas@gmail.com>
2023-05-01 22:46:09 +0900

    ScanCommand実行の関数ファイル作り直し

---
+++ src/aws/scanItemsFunc.ts
@@ -0,0 +1,21 @@
+import { ScanCommand, ScanCommandOutput } from '@aws-sdk/lib-dynamodb';
+import { ddbDocClient } from './ddbDocClient';
+
+const scanItemsFunc = async (): Promise<ScanCommandOutput> => {
+  const param = {
+    TableName: 'myWeightData',
+  };
+
+  try {
+    const data: ScanCommandOutput = await ddbDocClient.send(
+      new ScanCommand(param),
+    );
+    // ここで日付順になるようソートする
+    //data.Items?.sort();
+    return data;
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export default scanItemsFunc;
