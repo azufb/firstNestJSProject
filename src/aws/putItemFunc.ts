41ad22467503329812e78f99810be11c5f4189a0
azufb <curiousazunas@gmail.com>
2023-05-02 20:58:37 +0900

    DynamoDBへデータ追加の関数作成

---
+++ src/aws/putItemFunc.ts
@@ -0,0 +1,27 @@
+import { PutCommand, PutCommandOutput } from '@aws-sdk/lib-dynamodb';
+import { ddbDocClient } from './ddbDocClient';
+
+const putItemFunc = async (formData: any) => {
+  const params = {
+    TableName: 'myWeightData',
+    Item: {
+      date: formData.date,
+      timestamp: formData.timestamp,
+      weight: formData.weight,
+      bmi: formData.bmi,
+    },
+  };
+
+  try {
+    const data: PutCommandOutput = await ddbDocClient.send(
+      new PutCommand(params),
+    );
+    console.log(data);
+
+    return data;
+  } catch (err) {
+    console.log('err', err);
+  }
+};
+
+export default putItemFunc;
