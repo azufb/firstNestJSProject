63a70ecc8628b2c0181255d14f2a69b8c5dc4746
azufb <curiousazunas@gmail.com>
2023-05-02 22:45:51 +0900

    dynamoDB用のProviderやControllerをRoot Moduleが使えるようにする

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config'; // @nestjs/configはdotenvに依存。
+import { DynamoDBService } from './aws/dynamoDB.service';
+import { DynamoDBController } from './aws/dynamoDB.controller';
 
 // アプリケーションを構成するメタデータを記述する
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, DynamoDBController],
+  providers: [AppService, DynamoDBService],
 })
 export class AppModule {}
