e7f680dc3304adfe09b3c76265f964052345f881
azufb <curiousazunas@gmail.com>
2023-05-02 20:46:23 +0900

    @nestjs/configを使って環境変数読み込めるようにした

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config'; // @nestjs/configはdotenvに依存。
 
 // アプリケーションを構成するメタデータを記述する
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
