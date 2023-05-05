08d59ae41f135aeb2737d60ae84c423ab883cbbe
azufb <curiousazunas@gmail.com>
2023-05-05 20:18:02 +0900

    dynamoDB操作関係のモジュールをimportして使えるように

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config'; // @nestjs/configはdotenvに依存。
-import { DynamoDBService } from './aws/dynamoDB.service';
-import { DynamoDBController } from './aws/dynamoDB.controller';
 import { SampleController } from './sample/sample.controller';
 import { SampleService } from './sample/sample.service';
+import { DynamoDBModule } from './aws/dynamoDB.module';
 
 // アプリケーションを構成するメタデータを記述する
 
 @Module({
-  imports: [ConfigModule.forRoot()],
-  controllers: [AppController, DynamoDBController, SampleController],
-  providers: [AppService, DynamoDBService, SampleService],
+  imports: [DynamoDBModule],
+  controllers: [AppController, SampleController],
+  providers: [AppService, SampleService],
 })
 export class AppModule {}
