73e7c46a5edea232978e622fdd5252970d291ade
azufb <curiousazunas@gmail.com>
2023-05-06 11:08:22 +0900

    ConfigModule削除

--- src/aws/dynamoDB.module.ts
+++ src/aws/dynamoDB.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config'; // @nestjs/configはdotenvに依存。
 import { DynamoDBController } from './dynamoDB.controller';
 import { DynamoDBService } from './dynamoDB.service';
 
 @Module({
-  imports: [ConfigModule.forRoot()],
+  imports: [],
   controllers: [DynamoDBController],
   providers: [DynamoDBService],
 })
